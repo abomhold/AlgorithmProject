17edbe8f1f6a1561e577ad8309b9dd241205e7c2
austin bomhold <abomhold@uw.edu>
2024-06-28 22:49:01 -0700

    cleaning

--- brute_force.py
+++ brute_force.py
@@ -1,15 +1,16 @@
 import graph
 from graph import generate_points, Point, calculate_distance
 
+max_node_count: int = 10
+node_count: int
+node_array: list[Point]
+memo: dict[tuple[int, int], tuple[float, list[Point]]]
 
-def solve(pos: int, mask: int) -> tuple[float, list[Point]]:
+
+def find_shortest_path(pos: int, mask: int) -> tuple[float, list[Point]]:
     # Check if all positions have been visited this recursive call
     if mask == (1 << node_count) - 1:
         return calculate_distance(node_array[pos], node_array[0]), [node_array[0]]
-
-    # # Check if the current combination has been calculated before
-    # if (pos, mask) in memo:
-    #     return memo[(pos, mask)]
 
     best_distance = float('inf')
     best_path = []
@@ -17,7 +18,7 @@ def solve(pos: int, mask: int) -> tuple[float, list[Point]]:
     for j in range(node_count):
         # if the index is not in the mask,...
         if not (mask & (1 << j)):
-            current_distance, sub_path = solve(j, mask | (1 << j))
+            current_distance, sub_path = find_shortest_path(j, mask | (1 << j))
             current_distance += calculate_distance(node_array[pos], node_array[j])
 
             if current_distance < best_distance:
@@ -27,15 +28,10 @@ def solve(pos: int, mask: int) -> tuple[float, list[Point]]:
     return best_distance, best_path
 
 
-max_node_count: int = 20
-node_count: int
-node_array: list[Point]
-memo: dict[tuple[int, int], tuple[float, list[Point]]]
-
 if __name__ == '__main__':
     for i in range(4, max_node_count):
         node_count = i
         node_array = generate_points(node_count)
         memo = {}
-        cost, path = solve(0, 1)
+        cost, path = find_shortest_path(0, 1)
         print(f"{i},{cost},{graph.calculations},{path}")
--- figures.py
+++ figures.py
@@ -1,32 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.scale import LogScale
-
 from graph import Point
 
-
-def plot_points(points: list[tuple[int, int]]) -> None:
-    fig, ax = plt.subplots()
-    # ax.set_yscal;e
-
-    ax.set_yscale('log', base=2)
-    # ax.set_xscale('log', base=2)
-    # ax.set_ylim(ymin=0,ymax=40000000)
-    ax.set_xlim(xmin=3, xmax=20)
-
-    for current_point in points:
-        plt.stem(current_point[0], current_point[1], linefmt='grey', markerfmt='D', bottom=1.1)
-        # plt.stem(,current_point[0])
-    plt.show()
-
-
-# for testing
-data = [(4, 21), (5, 85), (6, 270), (7, 786), (8, 2179), (9, 5827), (10, 15124), (11, 38264), (12, 94705), (13, 230017),
-        (14, 549674), (15, 1295342), (16, 3015887), (17, 6948303), (18, 15861488), (19, 35915828)]
+dynamic = [21, 85, 270, 786, 2179, 5827, 15124, 38264, 94705]
+brute = [21, 88, 445, 2676, 18739, 149920, 1349289, 13492900, 148421911]
+x_values = [x for x in range(4, 13)]
 
 if __name__ == '__main__':
-    plot_points(data)
+    fig, ax = plt.subplots()
+    for index in range(len(x_values)):
+        plt.plot(x_values[index], dynamic[index], '1', color='blue')
 
+    for index in range(len(x_values)):
+        plt.plot(x_values[index], brute[index], '2', color='red')
+
+    ax.set_yscale('log', base=2)
+    plt.show()
+
+# plt.step(current_point[0], current_point[1], label='pre (default)')
 # fig, ax = plt.subplots()
 #
 # dt = 0.01
@@ -36,3 +28,29 @@ if __name__ == '__main__':
 # ax.grid()
 #
 # plt.show()
+
+# # for testing
+# # x_values = [x for x in range(4, 20)]
+# # dynamic = [21, 85, 270, 786, 2179, 5827, 15124, 38264, 94705, 230017, 549674, 1295342, 3015887, 6948303, 15861488,
+# #            35915828]
+# #
+# dynamic = [(4, 21), (5, 85), (6, 270), (7, 786), (8, 2179), (9, 5827), (10, 15124), (11, 38264), (12, 94705),
+#            (13, 230017), (14, 549674), (15, 1295342), (16, 3015887), (17, 6948303), (18, 15861488), (19, 35915828)]
+# #
+# brute = [(4, 21), (5, 88), (6, 445), (7, 2676), (8, 18739), (9, 149920), (10, 1349289), (11, 13492900), (12, 148421911)]
+
+# def plot_points(points: list[tuple[int, int]]) -> None:
+#     fig, ax = plt.subplots()
+#     # ax.set_yscal;e
+#     # ax.set_yscale('log', base=2)
+#     # ax.set_xscale('log', base=2)
+#     # ax.set_ylim(ymin=0,ymax=40000000)
+#
+#     # ax.set_xlim(xmin=3, xmax=20)
+#
+#     for current_point in points:
+#         plt.step(current_point[0], current_point[1], label='pre (default)')
+#         plt.plot(current_point[0], current_point[1], 'o--', color='blue')
+#         # plt.stem(current_point[0], current_point[1], linefmt='grey', markerfmt='D', bottom=1.1)
+#         # plt.stem(,current_point[0])
+#         # plt.stairs(current_point[0], current_point[1])
